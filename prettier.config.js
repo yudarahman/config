46c257a8d0b9586b6b51e79f2e332ced45209d79
yudarahman.garuda <yudarahman.jtk12@gmail.com>
2024-03-02 11:35:19 +0700

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "all",
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+};
+
+module.exports = config;
